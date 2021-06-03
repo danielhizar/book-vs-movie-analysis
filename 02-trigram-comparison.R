a9be9e30106f4dd0b42f50e6eaeee97ae02197fa
danielhizar <danielhizar94@gmail.com>
2021-06-03 16:50:26 +0300

    worked on the trigram comparison script

--- 02-trigram-comparison.R
+++ 02-trigram-comparison.R
@@ -2,5 +2,20 @@
 library(tidyverse)
 
 # read data in
-emma_book <- read_tsv("processed-data/emma-book.tsv")
-emma_script <- read_tsv("processed-data/emma-script.tsv")
+emma_book <- read_tsv("processed-data/emma-book.tsv") %>% 
+  mutate(source ="book")
+
+emma_script <- read_tsv("processed-data/emma-script.tsv") %>% 
+  mutate(source ="script")
+
+#create a mutate to add a column called "paragraph" to both data frames that contain paragraph number
+
+emma_book <- emma_book %>% 
+  mutate(paragraph = rownames(emma_book))
+
+emma_script <- emma_script %>% 
+  mutate(paragraph = rownames(emma_script))
+
+# combine both corpora into one dataframe
+emma_corpora <- bind_rows(emma_book, emma_script) %>% 
+  rename(text = ".")
